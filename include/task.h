609b27dc365fe578442760daddd9b26bce79afbb
cos-public <nowhere@example.com>
2018-08-11 00:31:23 +0300

    debug output removed

--- include/task.h
+++ include/task.h
@@ -1,11 +1,7 @@
 #ifndef TASK_H
 #define TASK_H
 
-#include <iostream>
 #include <experimental/coroutine>
-#include <experimental/resumable>
-
-static int task_counter = 0;
 
 
 template<typename T> class task;
@@ -14,12 +10,9 @@ template<typename T> class task;
 class task_promise_base {
 public:
 	std::experimental::suspend_never initial_suspend() noexcept {
-		std::cout << "task::promise_type::initial_suspend()\n";
 		return {};
 	}
 	auto final_suspend() noexcept {
-		std::cout << "task::promise_type::final_suspend()\n";
-
 		struct final_awaiter {
 			std::experimental::coroutine_handle<> waiter;
 			bool await_ready() { return false; }
@@ -34,12 +27,10 @@ public:
 	}
 
 	void set_waiter(std::experimental::coroutine_handle<> & waiter) {
-		std::cout << "task::promise_type::set_waiter() " << "\n";
 		_waiter = waiter;
 	}
 
 	void unhandled_exception() noexcept {
-		std::cout << "task::promise_type::unhandled_exception()\n";
 		_e = std::current_exception();
 	}
 
@@ -58,7 +49,6 @@ public:
 	task<T> get_return_object() noexcept;
 
 	void return_value(T value) {
-		std::cout << "task::promise_type::return_value()\n";
 		m_value = value;
 	}
 
@@ -81,7 +71,6 @@ public:
 	task<void> get_return_object() noexcept;
 
 	void return_void() {
-		std::cout << "task::promise_type::return_void()\n";
 	}
 
 	void result() {
@@ -97,11 +86,6 @@ public:
 	using promise_type = task_promise<T>;
 
 	explicit task(std::experimental::coroutine_handle<promise_type> & coro) : _coro(coro) {
-		std::cout << "task()" << " " << _id << std::endl;
-	}
-
-	~task() {
-		std::cout << "~task()" << " " << _id << std::endl;
 	}
 
 	bool await_ready() {
@@ -111,7 +95,6 @@ public:
 	//another coroutine wants to co_await this coroutine.
 	//set it to promise object, so it will continue at final_suspend point
 	void await_suspend(std::experimental::coroutine_handle<> waiter) {
-		std::cout << "task::await_suspend() " << _id << "\n";
 		_coro.promise().set_waiter(waiter);
 	}
 
@@ -119,13 +102,11 @@ public:
 	//this returns the result to the awaiting coroutine's co_await. we get result from
 	//this coroutine's promise
 	T await_resume() {
-		std::cout << "task::await_resume() " << _id << "\n";
 		return _coro.promise().result();
 	}
 
 private:
 	std::experimental::coroutine_handle<promise_type> _coro;
-	int _id = task_counter++;
 };
 
 
